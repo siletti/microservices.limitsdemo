3e05b59a0515284e1319f3f8e95f90766bfbb071
Alberto Siletti <alberto.siletti@gmail.com>
2019-06-02 16:25:59 +0200

    add @ConfigurationProperties

--- src/main/java/com/siletti/microservices/limitsdemo/LimitsConfigurationController.java
+++ src/main/java/com/siletti/microservices/limitsdemo/LimitsConfigurationController.java
@@ -1,15 +1,19 @@
 package com.siletti.microservices.limitsdemo;
 
 import com.siletti.microservices.limitsdemo.bean.LimitConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsConfigurationController {
 
+    @Autowired
+    Configuration configuration;
+
     @GetMapping("/limits")
     public LimitConfiguration getLimitsConfig(){
 
-        return new LimitConfiguration(1000, 1) ;
+        return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum()) ;
     }
 }
